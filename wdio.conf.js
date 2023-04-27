fd7f6594d761b75b75842611072fa0d67949571d
Nancy Rachel <nancystanley92@gmail.com>
2023-04-27 12:46:55 +0530

    Adding headless Argument

--- wdio.conf.js
+++ wdio.conf.js
@@ -59,6 +59,11 @@ exports.config = {
       //
       browserName: "chrome",
       acceptInsecureCerts: true,
+      'goog:chromeOptions': {
+        args: ['--headless', '--disable-gpu']
+      }
+
+ 
       // If outputDir is provided WebdriverIO can capture driver session logs
       // it is possible to configure which logTypes to include/exclude.
       // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
