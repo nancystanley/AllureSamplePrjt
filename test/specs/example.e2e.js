6a423841a477424ad48e6e21b2d5a98a2514aa53
Nancy Rachel <nancystanley92@gmail.com>
2023-04-27 12:10:29 +0530

    Changes in Spec File

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,6 +1,6 @@
 const LoginPage = require('../pageobjects/login.page');
 const SecurePage = require('../pageobjects/secure.page');
-//
+//Describe block
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
         await LoginPage.open();
