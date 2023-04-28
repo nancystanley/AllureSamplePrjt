64a22fc31098a74369a0150641aefb9dca77096e
Nancy Rachel <nancystanley92@gmail.com>
2023-04-28 17:08:16 +0530

    After running

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,12 +1,13 @@
 const LoginPage = require('../pageobjects/login.page');
 const SecurePage = require('../pageobjects/secure.page');
 //Describe block begins
+//You logged into a secure area!
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
         await LoginPage.open();
         await LoginPage.login('tomsmith', 'SuperSecretPassword!');
         await expect(SecurePage.flashAlert).toBeExisting();
-        await expect(SecurePage.flashAlert).toHaveTextContaining('You logged into a secure area!');
+        await expect(SecurePage.flashAlert).toHaveTextContaining('hai');
     });
 });
 
